b1b2a5438d105cc82d66015f91cdf1fc39df8133
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-02-28 19:56:07 +0530

    Updated SecurityConfig: Removed form login, enabled basic auth

---
+++ src/main/java/com/example/SPRING_SECURITY_6/Config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.SPRING_SECURITY_6.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests ->
+                        requests.anyRequest().authenticated() // Ensure all requests need authentication
+                )
+//                .formLogin(Customizer.withDefaults()) // Enable form-based login
+                .httpBasic(Customizer.withDefaults()); // Enable basic authentication
+
+        return http.build();
+    }
+
+}
