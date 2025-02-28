16728888a736e938e4d0041d8f56611c81c7878a
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-02-28 22:07:25 +0530

    Added role-based authorization with @PreAuthorize

--- src/main/java/com/example/SPRING_SECURITY_6/Controller/GreetingController.java
+++ src/main/java/com/example/SPRING_SECURITY_6/Controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.SPRING_SECURITY_6.Controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +13,13 @@ public class GreetingController {
         return "Hello";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     public String userPage() {
         return "Hello, User!";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String adminPage() {
         return "Hello, Admin!";
