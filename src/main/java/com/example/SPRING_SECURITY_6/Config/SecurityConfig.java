62d5cc662627f2b0b7fa4f1ad66da62badc9e271
SURESH MUKHI <139912199+sureshmukhi369@users.noreply.github.com>
2025-02-28 21:53:10 +0530

    Added in-memory authentication with user1 and admin

--- src/main/java/com/example/SPRING_SECURITY_6/Config/SecurityConfig.java
+++ src/main/java/com/example/SPRING_SECURITY_6/Config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +28,24 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults()); // Enable basic authentication
 
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("pass1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("pass")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
